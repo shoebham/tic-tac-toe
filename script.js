a8745389d709d32b5816f1982519d689ffa91eb3
Shubham <25881429+shoebham@users.noreply.github.com>
2021-08-04 20:39:16 +0530

    Added draw and game over functionality

--- script.js
+++ script.js
@@ -4,9 +4,9 @@
 //     ['o','o','x']
 // ];
 let arr=[
-    ["00","01","02"],
-    ["10","11","12"],
-    ["20","21","22"]
+    ["","",""],
+    ["","",""],
+    ["","",""]
 ];
 const gameBoard = (()=>
 {
@@ -15,6 +15,7 @@ const gameBoard = (()=>
         fillBoard();
         _clear();
         bind();
+        
     }
     const fillBoard = (x,y,piece)=>
     {
@@ -40,6 +41,12 @@ const gameBoard = (()=>
                 ["","",""],
                 ["","",""]];
             fillBoard();
+            let cells=document.querySelectorAll(".cell");
+            cells.forEach((e)=>
+            {
+                e.disabled=false    ;
+                e.style=""
+            })
         });
     }
     const bind = ()=>
@@ -77,22 +84,64 @@ const gameBoard = (()=>
             // console.log("winner: ",piece )    
         }
     
-        return "";
+  
         // console.log("Draw");
-        //diagonal
-        // for(var i=0;i<arr.length;i++)
-        // {
-        //     if(arr[i][y]!=piece)
-        //         break;
-        //     if(i==arr.length-1)
-        //         return piece;   
-        //     // console.log("winner: ",piece )    
-        // }
+        // diagonal
+        for(var i=0;i<arr.length;i++)
+        {
+            if(arr[i][i]!=piece)
+                break;
+            if(i==arr.length-1)
+                return piece;   
+            // console.log("winner: ",piece )    
+        }
+        
+        return "";
     }
 
     const _declare_winner =(piece)=>
     {
+        _gameover(piece);
+        _board_full();
         document.querySelector("#winner").textContent=piece;
+        console.log("in declare winner",piece)
+    }
+    const _board_full = ()=>
+    {
+        let flag=false;
+        for(var i=0;i<arr.length;i++)
+        {
+            for(var j=0;j<arr.length;j++)
+            {
+                if(arr[i][j]=="")
+                {
+                    flag=false;
+                    break;
+                }
+                else
+                {
+                    flag=true;
+                }
+            }
+        }
+        console.log(flag);
+        if(flag)
+        {
+            document.querySelector("#draw").textContent="It's a draw";
+            _gameover(flag);
+        }
+    }
+    const _gameover = (x)=>
+    {
+        if(x)
+        {
+            let cells=document.querySelectorAll(".cell");
+            cells.forEach((e)=>
+            {
+                e.disabled=true;
+                e.style=""
+            })
+        }
     }
     return {init,bind,fillBoard};
 })();
@@ -101,17 +150,49 @@ const gameBoard = (()=>
 const player =  ((piece)=>{
     
     let curr_player="X";
-    const _player_choice =()=>
+   
+    const player_choice =()=>
     {
-        player1:"X";
-        player2:"O";
-    }
+        console.log("here");
+        let player1= document.querySelector("#player1_sel");
+        let player2= document.querySelector("#player2_sel");
+        if(player1.value=="X")
+        {player2.value="O";}
+        else if(player1.value=="O")
+        {player2.value="X";}
+        curr_player=player1.value;
+        console.log("here",curr_player)
+        return curr_player;
+    };
     const _curr_piece=(()=>{
-     if(curr_player=="X"){
-         curr_player="O";
-     }else{curr_player="X"}
-     
-     return curr_player;
+        if(curr_player=="X")
+        curr_player="O";
+        else
+        curr_player="X"
+        player_toggle();
+        return curr_player;
+    });
+
+
+    const player_toggle=(()=>
+    {
+        let player1=document.querySelector("#player1");
+        let player2=document.querySelector("#player2");
+        let player1_sel= document.querySelector("#player1_sel").value;
+        let player2_sel= document.querySelector("#player2_sel").value;
+        console.log("in player toggle",curr_player)
+        if(player1_sel==curr_player)
+        {            
+            player1.classList.toggle("curr_sel");
+            player2.classList.toggle("curr_sel");
+
+        }   
+        if(player2_sel==curr_player)
+        {
+            player1.classList.toggle("curr_sel");
+            player2.classList.toggle("curr_sel");
+        }
+
     });
     const move =(e)=>
     {
@@ -119,19 +200,28 @@ const player =  ((piece)=>{
         const i =e.substring(5,7)[0];
         const j =e.substring(5,7)[1];
         // console.log(i,j);
-        let piece;
+        let piece=curr_player;
         console.log(piece);
         if(arr[i][j]==="")
         {
-            piece = _curr_piece();
             arr[i][j]=piece;
             gameBoard.fillBoard(i,j,piece);         
             console.log(arr)   
+            piece = _curr_piece();
         }
         // gameBoard.check_winner(i,j,piece);
         
     };
-    return {move,curr_player};
+
+    const _bind_selector=(()=>
+    {
+        let player1= document.querySelector("#player1_sel");
+        let player2= document.querySelector("#player2_sel");
+        console.log("here");
+        player1.addEventListener("change",()=>{ player_choice()});
+        player2.addEventListener("change",()=>{ player_choice()});
+    })();
+    return {move,player_choice,player_toggle};
 })();
 
 gameBoard.init();
